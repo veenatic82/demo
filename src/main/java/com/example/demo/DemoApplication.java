9d137012b4eca23c1ec9c57089d1a7ce77af9866
PK <praveen.bit2k4@gmail.com>
2021-04-23 01:25:18 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+@RestController
+class AppController {
+	@RequestMapping("hello/{name}")
+	public Mono<String> getHello(@PathVariable("name") String name) {
+		return Mono.just("Hello " + name);
+	}
+}
